61e2ebe80a1191a993b952e38d3822ea5cf181b9
T1T3 <sdgeara@yahoo.com>
2021-08-22 17:09:58 +0900

    Add files via upload

--- labelimg/video2img.py
+++ labelimg/video2img.py
@@ -6,42 +6,45 @@
 
 import cv2
 
-video_path = r'C:\Users\Zman\PycharmProjects\yolov5\labelimg\video'
-out_path = r'C:/Users/Zman/PycharmProjects/yolov5/labelimg/images/'   # \ ---> /
+
+def save_image(addr, image, num):
+    address = addr + 'img_' + str(num) + '.jpg'  # \ ---> /
+    print(address)
+    cv2.imwrite(address, image)
 
 
-is_all_frame = True #すべてフレームを取ります
-sta_frame = 1       #始めいてフレーム
-end_frame = 1000    #最後のフレーム
+video_path = r'C:\Users\Zman\PycharmProjects\yolov5\labeling\video\train3.flv'
+out_path = r'C:/Users/Zman/PycharmProjects/yolov5/labeling/images/'  # \ ---> /
 
-time_interval = 8   #時間の間隔
+is_all_frame = True  # すべてフレームを取ります
+sta_frame = 1  # 始めいてフレーム
+end_frame = 1000  # 最後のフレーム
+
+time_interval = 8  # 時間の間隔
 
 videocapture = cv2.VideoCapture(video_path)
-#pip install opencv-contrib-python
+# pip install opencv-contrib-python
 
-success,frame = videocapture.read()
-print(success,frame)
+success, frame = videocapture.read()
 
-def save_image(addr,image,num):
-    address=addr+'img_'+str(num)+'.jpg'   # \ ---> /
-    print(address)
-    cv2.imwrite(address,image)
+# print(success,frame)
 
-i=0
-j=0
+
+i = 0
+j = 0
 while success:
-    i+=1
+    i += 1
     if i % time_interval == 0:
         if not is_all_frame:
             if sta_frame <= i <= end_frame:
                 j += 1
-                print('save frame',j)
-                save_image(out_path,frame,j)
+                print('save frame', j)
+                save_image(out_path, frame, j)
             elif i > end_frame:
                 break
         else:
-            j+=i
-            print('save frame',j)
-            save_image(out_path,frame,j)
+            j += 1
+            print('save frame', j)
+            save_image(out_path, frame, j)
 
-    success,frame=videocapture.read()
+    success, frame = videocapture.read()
