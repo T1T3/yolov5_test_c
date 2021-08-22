2f5ff665f2a43e894494d0b0817f753409237b9f
T1T3 <sdgeara@yahoo.com>
2021-08-22 17:08:59 +0900

    Add files via upload

--- aim-csgo/cs_model.py
+++ aim-csgo/cs_model.py
@@ -7,19 +7,20 @@
 import torch
 from models.experimental import attempt_load
 
-#detect.py
+# detect.py
 
-device='cuda' if torch.cuda.is_available() else 'cpu'
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 half = device != 'cpu'
 
-weights=r''
-imgsz=640
+weights = r'C:\Users\Zman\PycharmProjects\yolov5\aim-csgo\models\yolov5s.pt'
+imgsz = 640
+
 
 def load_model():
-    #p76
+    # p76
     model = attempt_load(weights, map_location=device)  # load FP32 model
     if half:
         model.half()  # to FP16
-    if device!='cpu':
-        model(torch.zeros(1,3,imgsz,imgsz).to(device).type_as(next(model.parameters())))
+    if device != 'cpu':
+        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))
     return model
